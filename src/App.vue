ccc51d57e4a777b9840d35cf39756e857111d93c
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-04-13 09:34:29 -0300

    pr2

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,56 @@ import 'bootstrap'
     <label for="endereço" class="form-label" >Endereço:</label>
     
   </div>
+<div class="form-floating mt-3 mb-3" >
+  <input type="text" class="form-control" id="Cidade" placeholder="Enter cidade " name="Cidade">
+  <label for="Cidade" class="form-label">Cidade</label>
+</div>
+
+ <div><label for="browser" class="form-label">Estado:</label>
+<input class="form-control" list="browsers" name="browser" id="browser">
+<datalist id="browsers">
+  <option value="Acre">AC</option>
+  <option value="Alagoas">AL</option>
+  <option value="Amapá">AP</option>
+  <option value="Amazonas">AM</option>
+  <option value="Bahia">BH</option>
+  <option value="Ceará">CE</option>
+  <option value="Distrito Federal">DF</option>
+  <option value="Espírito Santo">ES</option>
+  <option value="Goiás">GO</option>
+  <option value="Maranhão">MA</option>
+  <option value="Mato Grosso">MT</option>
+  <option value="Mato Grosso do Sul">MS</option>
+  <option value="Minas Gerais">MG</option>
+  <option value="Pará">PA</option>
+  <option value="Paraíba">PB</option>
+  <option value="Paraná">PR</option>
+  <option value="Pernambuco">PE</option>
+  <option value="Piaui">PI</option>
+  <option value="Rio de Janeiro">RJ</option>
+  <option value="Rio Grande do Norte">RN</option>
+  <option value="Rio Grande do Sul">RS</option>
+  <option value="Rondônia">RO</option>
+  <option value="Roraima">RR</option>
+  <option value="Santa Catarina">SC</option>
+  <option value="São Paulo">SP</option>
+  <option value="Sergipe">SE</option>
+  <option value="Tocantins">TO</option>  
+</datalist>
+</div>
+
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Hobbies</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Linguagens de Programação</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Biografia</label>
+</div>
   <div class="Relembrar">
     <label class="form-check-label">
       <input class="form-check-input" type="checkbox" name="remember"> Lembre-me 
